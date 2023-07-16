9e653561f147f9043b7d9c10e89689f402ff17ec
Imran Ansari <imranansari7248@gmail.com>
2023-07-16 20:40:01 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Ubuntu': ['Ubuntu', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
